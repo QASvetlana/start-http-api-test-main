d23a31f616527fb8292279424556f347b1a44c23
Sveta Andreeva <89111378147v@yandex.com>
2023-09-23 10:30:48 +0300

    1

---
+++ specs/config.js
@@ -0,0 +1,14 @@
+//сюда выносим данные, которые будем использовать
+const config = {
+    url: 'https://try.vikunja.io', // base url
+    credentials: {
+        username: 'demo',
+        password: 'demo'
+}
+}
+export default config; // делаем возможность экспорта
+
+/*
+process.env/API_USERNAME
+process.env/API_PASSWORD
+*/ 
